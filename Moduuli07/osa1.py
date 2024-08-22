64f04c06ddd68151ff56b64e9220fed78f958796
roflvapor <prokevingaming@gmail.com>
2024-08-22 17:55:00 +0300

    moduuli07 up to osa 2

---
+++ Moduuli07/osa1.py
@@ -0,0 +1,4 @@
+kknumero = int(input("anna kuukauden numero: "))
+vuosajat = ("talvi","talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy","talvi")
+kk = vuosajat[kknumero-1]
+print(kk)
