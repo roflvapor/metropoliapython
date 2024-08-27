640aff566475612d721e67fc49b5919f39208418
roflvapor <prokevingaming@gmail.com>
2024-08-27 13:33:21 +0300

    osa3

--- Moduuli07/osa3.py
+++ Moduuli07/osa3.py
@@ -1,21 +1,19 @@
 
-syote = int(input("Haluatko 1: tallenna uusi lentoasema, 2: hakea lentoasema, 3: lopettaa "))
+syote = int(input("\nHaluatko 1: tallenna uusi lentoasema, 2: hakea lentoasema, 3: lopettaa "))
+lentoasemat = {}
+
 while syote != 3:
-    lentoasemat = {"20":"caca"}
     if syote == 1:
         airfield_icao = input("Anna lentoaseman ICAO koodi: ")
         airfield_name = input("Anna lentoaseman nimi: ")
-        if airfield_icao or airfield_name in lentoasemat.keys():
+        if airfield_icao in lentoasemat:
             print("Lentoasema on jo tallennettu!")
-        elif airfield_icao or airfield_name not in lentoasemat:
+        elif airfield_icao not in lentoasemat:
             lentoasemat[airfield_icao] = airfield_name
     if syote == 2:
         airfield_icao = input("Anna lentoaseman ICAO koodi: ")
-        for icao,name in lentoasemat:
+        for icao in lentoasemat:
             if icao == airfield_icao:
                 print(f"Lentoaseman nimi on {lentoasemat[icao]}")
 
-
-    if syote == 3:
-        break
-    syote = int(input("Haluatko 1: tallenna uusi lentoasema, 2: hakea lentoasema, 3: lopettaa "))
+    syote = int(input("\nHaluatko 1: tallenna uusi lentoasema, 2: hakea lentoasema, 3: lopettaa "))
